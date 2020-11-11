9bd9194c61493a5eea18c6750901aed81e4b38db
randall.erasmus <randallerasmus1@gmail.com>
2020-11-12 00:20:57 +0200

    Setting up the backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+
+app.get('/',(req,res) =>{
+    res.send('API is running......')
+})
+
+// Endpoint to return all the products --> Setting up some routes
+app.get('/api/products',(req,res) =>{
+    res.json(products)
+})
+// Endpoint to return a single product
+app.get('/api/products/:id',(req,res) =>{
+    const product = products.find(p =>p._id === req.params.id)
+    res.json(product)
+})
+
+app.listen(5000, console.log('server running on port 5000'))
